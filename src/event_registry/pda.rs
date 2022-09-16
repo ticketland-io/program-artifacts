82a1321ac4ca41ded18f8d995183893ac9e34069
ppoliani <ppoliani@gmail.com>
2022-09-16 10:44:36 +0300

    feat: add event_nft pda

--- src/event_registry/pda.rs
+++ src/event_registry/pda.rs
@@ -2,6 +2,13 @@ use solana_sdk::{
   pubkey::Pubkey,
 };
 
+pub fn event_nft(state: &Pubkey, event_id: [u8; 32]) -> (Pubkey, u8) {
+  Pubkey::find_program_address(
+    &[b"event_nft", state.as_ref(), &event_id],
+    &super::program_id(),
+  )
+}
+
 pub fn event_nft_authority(state: &Pubkey) -> (Pubkey, u8) {
   Pubkey::find_program_address(
     &[b"event_nft_authority", state.as_ref()],
