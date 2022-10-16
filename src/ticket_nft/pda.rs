3ddbb6061bd1cb873bd454a89117e87f60b5ca6f
ppoliani <ppoliani@gmail.com>
2022-10-16 12:11:26 +0300

    feat: change event_id to str

--- src/ticket_nft/pda.rs
+++ src/ticket_nft/pda.rs
@@ -5,11 +5,11 @@ use solana_sdk::{
 pub fn ticket_nft(
   state: &Pubkey,
   seat_index: u32,
-  event_id: [u8; 32],
+  event_id: &str,
   ticket_type_index: u8,
 ) -> (Pubkey, u8) {
   Pubkey::find_program_address(
-    &[b"ticket_nft", state.as_ref(), seat_index.to_string().as_ref(), &event_id, ticket_type_index.to_string().as_ref()],
+    &[b"ticket_nft", state.as_ref(), seat_index.to_string().as_ref(), event_id.as_ref(), ticket_type_index.to_string().as_ref()],
     &super::program_id(),
   )
 }
