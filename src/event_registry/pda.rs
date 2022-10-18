0b31b05d483c83ef66578afe82057c4301d7abd4
ppoliani <ppoliani@gmail.com>
2022-10-18 14:40:13 +0300

    feat: add event_nft_metadata pda

--- src/event_registry/pda.rs
+++ src/event_registry/pda.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
 use solana_sdk::{
   pubkey::Pubkey,
 };
+
+const METAPLEX_ID: &str = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
 
 pub fn event(state: &Pubkey, event_id: &str) -> (Pubkey, u8) {
   Pubkey::find_program_address(
@@ -20,5 +23,13 @@ pub fn event_nft_authority(state: &Pubkey) -> (Pubkey, u8) {
   Pubkey::find_program_address(
     &[b"event_nft_authority", state.as_ref()],
     &super::program_id(),
+  )
+}
+
+// Metaplex PDA
+pub fn event_nft_metadata(event_nft: &str) -> (Pubkey, u8) {
+  Pubkey::find_program_address(
+    &[b"metadata", METAPLEX_ID.as_bytes(), event_nft.as_bytes()],
+    &Pubkey::from_str(METAPLEX_ID).unwrap(),
   )
 }
