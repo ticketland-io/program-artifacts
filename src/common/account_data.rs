f900ec4ca161758f000f97c1e46fa6d8ad87ee08
ppoliani <ppoliani@gmail.com>
2022-11-07 15:43:04 +0200

    chore: import Slot from solana sdk

--- src/common/account_data.rs
+++ src/common/account_data.rs
@@ -1,5 +1,8 @@
 use borsh::{BorshSerialize, BorshDeserialize};
-use solana_sdk::pubkey::Pubkey;
+use solana_sdk::{
+  pubkey::Pubkey,
+  slot_history::Slot,
+};
 
 #[derive(BorshSerialize, BorshDeserialize, Clone, Default)]
 pub struct Reservation {
