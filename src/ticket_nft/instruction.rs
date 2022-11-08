eb21eaefe2fb431f3d81e8152a0f86fd14e991d1
Pavlos Polianidis <ppoliani@gmail.com>
2022-11-08 20:24:21 +0200

    Update instruction.rs

--- src/ticket_nft/instruction.rs
+++ src/ticket_nft/instruction.rs
@@ -10,13 +10,8 @@ pub struct SetTicketAttendedIx {
   pub ticket_metadata: Pubkey,
 }
 
-// We follow the same approach that Anchor does. The reason is that we need to provide the instuction code as part of the tx 
-// data so when it hits Anchor program it know which ix processor to call.
-// If we run anchor expand we can see similar code to this
 impl InstructionData for SetTicketAttendedIx {
   fn data(&self) -> Vec<u8> {
-    // Check how this is calculate here https://github.com/coral-xyz/anchor/blob/698426033052781988cd7980249726501ae08bdc/lang/syn/src/codegen/program/instruction.rs#L137
-    // Instead we just copied the value from the code Anchor has generated after running Anchor expand.
     let mut d = [167, 44, 23, 56, 62, 140, 237, 135].to_vec();
     d.append(&mut self.try_to_vec().expect("Should always serialize"));
     d
